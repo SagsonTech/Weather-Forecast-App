475326fb86ae1f5b6a39fbc940866ccbe47eacdc
SagsonTech <sagsontech26@gmail.com>
2024-01-18 21:10:23 +0530

    Completed Front-end

---
+++ script.js
@@ -0,0 +1,14 @@
+// const API_KEY = 'd8a9c794d1587dcb15c135704d8eff08';
+// const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+// let fetchData = async (city) => {
+//     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+//     let data = await response.json();
+//     return data;
+// }
+
+// fetchData('Chennai').then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.log(error);
+// })
